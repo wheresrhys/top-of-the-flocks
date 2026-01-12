6f1b1fc199fcb35c8368328f83215426ec6cc1e7
Rhys Evans <wheresrhys@gmail.com>
2026-01-12 18:36:15 +0000

    remove new data tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,131 +1,6 @@
 import { Container, Box, Typography, Grid } from '@mui/material';
 import AllTimeLeagueTableTabs from './components/AllTimeLeagueTableTabs';
-import {
-	getLeagueTableData,
-	TemporalUnit,
-	type LeagueTableConfig,
-	LeagueTableDisplay
-} from './components/LeagueTable';
-
-const monthNameMap = [
-	null,
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December'
-];
-
-async function MonthStats({
-	month,
-	year,
-	heading
-}: {
-	month: number;
-	year: number;
-	heading: string;
-}) {
-	const [
-		bestDaysThisMonth,
-		bestDaysThisMonthInAnyYear,
-		bestDaysThisYear,
-		bestThisMonthInAnyYear
-	] = await Promise.all([
-		getLeagueTableData({
-			temporalUnit: 'day' as TemporalUnit,
-			numberOfEntries: 5,
-			monthFilter: month,
-			yearFilter: year
-		}),
-		getLeagueTableData({
-			temporalUnit: 'day' as TemporalUnit,
-			numberOfEntries: 5,
-			monthFilter: month
-		}),
-		getLeagueTableData({
-			temporalUnit: 'day' as TemporalUnit,
-			numberOfEntries: 5,
-			yearFilter: year
-		}),
-		getLeagueTableData({
-			temporalUnit: 'month' as TemporalUnit,
-			numberOfEntries: 5,
-			monthFilter: month
-		})
-	]);
-
-	return (
-		<Box>
-			<Typography
-				variant="h3"
-				component="h2"
-				sx={{
-					mb: 4,
-					textAlign: 'left'
-				}}
-			>
-				{heading}
-			</Typography>
-			<LeagueTableDisplay
-				data={bestDaysThisMonth}
-				heading="Best days"
-				config={
-					{
-						temporalUnit: 'day'
-					} as LeagueTableConfig
-				}
-			/>
-			<LeagueTableDisplay
-				data={bestDaysThisMonthInAnyYear}
-				heading={`Best ${monthNameMap[month]} days ever`}
-				config={
-					{
-						temporalUnit: 'day'
-					} as LeagueTableConfig
-				}
-			/>
-			{/* TODO: Split the year data out into a separate thing */}
-			<LeagueTableDisplay
-				data={bestDaysThisYear}
-				heading={`Best days this year`}
-				config={
-					{
-						temporalUnit: 'day'
-					} as LeagueTableConfig
-				}
-			/>
-			<LeagueTableDisplay
-				data={bestThisMonthInAnyYear}
-				heading={`Best ever ${monthNameMap[month]}`}
-				config={
-					{
-						temporalUnit: 'month'
-					} as LeagueTableConfig
-				}
-			/>
-		</Box>
-	);
-}
-
-function getMonthAndYear(date: Date): { month: number; year: number } {
-	return { month: date.getMonth() + 1, year: date.getFullYear() };
-}
-
-function getLastMonthAndYear(date: Date): { month: number; year: number } {
-	const { month, year } = getMonthAndYear(date);
-	const lastMonth = month - 1;
-	if (lastMonth === 0) {
-		return { month: 12, year: year - 1 };
-	}
-	return { month: lastMonth, year };
-}
+import { getLeagueTableData, TemporalUnit } from './components/LeagueTable';
 
 export default async function Home() {
 	// Fetch the first tab's data server-side
@@ -133,8 +8,6 @@ export default async function Home() {
 		temporalUnit: 'day' as TemporalUnit,
 		numberOfEntries: 10
 	});
-
-	const today = new Date();
 
 	return (
 		<Container maxWidth="xl">
@@ -150,17 +23,7 @@ export default async function Home() {
 			</Typography>
 
 			<Grid container spacing={2}>
-				<Grid size={{ xs: 12, md: 6 }}>
-					<MonthStats
-						{...getMonthAndYear(today)}
-						heading="This month's records"
-					/>
-
-					<MonthStats
-						{...getLastMonthAndYear(today)}
-						heading="Last month's records"
-					/>
-				</Grid>
+				<Grid size={{ xs: 12, md: 6 }}></Grid>
 				<Grid size={{ xs: 12, md: 6 }}>
 					<Box>
 						<Typography
