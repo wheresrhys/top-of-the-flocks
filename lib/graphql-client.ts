0b9e54e267a4140154f1ba443c080c0136e71a8a
Rhys Evans <wheresrhys@gmail.com>
2026-01-15 15:34:41 +0000

    remove page caching

--- lib/graphql-client.ts
+++ lib/graphql-client.ts
@@ -47,6 +47,7 @@ export async function graphqlRequest<T = unknown>(
   return limit(async () => {
     try {
       const response = await fetch(hasuraConfig.endpoint, {
+        cache: 'no-store',
         method: 'POST',
         headers: hasuraHeaders,
         body: JSON.stringify({
