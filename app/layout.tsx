381b2a27ba97a41c5f31533bc43739885178f135
Rhys Evans <wheresrhys@gmail.com>
2026-01-22 19:50:40 +0000

    Revert to using material-ui for all styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
 import type { Metadata } from 'next';
+import './globals.css';
+import ThemeProvider from './components/ThemeProvider';
+import EmotionRegistry from '../lib/emotion-registry';
 
 export const metadata: Metadata = {
-	title: 'Top of the Flocks',
-	description: 'Leaderboard for bird ringing data'
+  title: 'Top of the Flocks',
+  description: 'Leaderboard for bird ringing data'
 };
 
 export default function RootLayout({
-	children
+  children
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <EmotionRegistry>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </EmotionRegistry>
+      </body>
+    </html>
+  );
 }
--- app/v1/layout.tsx
+++ app/v1/layout.tsx
@@ -1,13 +1,4 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import ThemeProvider from './components/ThemeProvider';
-import EmotionRegistry from '../../lib/emotion-registry';
 import GlobalNav from './components/GlobalNav';
-
-export const metadata: Metadata = {
-	title: 'Top of the Flocks',
-	description: 'Leaderboard for bird ringing data'
-};
 
 export default function RootLayout({
 	children
@@ -15,15 +6,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body>
-				<EmotionRegistry>
-					<ThemeProvider>
+		<>
 						<GlobalNav />
 						{children}
-					</ThemeProvider>
-				</EmotionRegistry>
-			</body>
-		</html>
+					</>
 	);
 }
