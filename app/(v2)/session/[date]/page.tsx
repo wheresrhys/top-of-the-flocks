1c8ee31fb6bda6efe4cd0cbc15f69408892d7adb
Rhys Evans <wheresrhys@gmail.com>
2026-01-24 16:05:28 +0000

    basic display of session data

--- app/(v2)/session/[date]/page.tsx
+++ app/(v2)/session/[date]/page.tsx
@@ -4,7 +4,9 @@ import gql from 'graphql-tag';
 import { unstable_cache } from 'next/cache';
 import { GetSessionByDateQuery } from '@/types/graphql.types';
 import { notFound } from 'next/navigation';
-
+import { List, ListItem, ListItemText, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+type SessionData = NonNullable<GetSessionByDateQuery['sessions']>[number];
+type EncounterData = NonNullable<SessionData['encounters']>[number];
 const GET_SESSION_BY_DATE = gql`
 	query GetSessionByDate($date: Date) {
 		sessions(where: { visitDate: { _eq: $date } }) {
@@ -26,6 +28,62 @@ const GET_SESSION_BY_DATE = gql`
 		}
 	}
 `;
+
+function getSpeciesBreakdown(session: SessionData) {
+	const map: Record<string, EncounterData[]> = {};
+	session.encounters.forEach(encounter => {
+		const species = encounter.bird?.species?.speciesName;
+		map[species] = map[species] || []
+		map[species].push(encounter);
+	});
+	return Object.entries(map).map(([species, encounters]) => ({ species, encounters })).sort((a, b) => {
+		if (a.encounters.length === b.encounters.length) {
+			return 0;
+		}
+		return a.encounters.length < b.encounters.length ? 1 : -1;
+	});
+}
+
+function SessionSummary({ session, date }: { session: SessionData, date: string }) {
+
+	const speciesBreakdown = getSpeciesBreakdown(session);
+	return (
+		<div>
+			<h1>
+			Session on {date}
+		</h1>
+		<List>
+				<ListItem><ListItemText>{session.encounters?.length} birds</ListItemText></ListItem>
+		<ListItem><ListItemText>{speciesBreakdown.length} species</ListItemText></ListItem>
+		<ListItem><ListItemText>{session.encounters.filter(encounter => encounter.recordType === 'N').length} new</ListItemText></ListItem>
+		<ListItem><ListItemText>{session.encounters.filter(encounter => encounter.recordType === 'S').length} retraps</ListItemText></ListItem>
+		<ListItem><ListItemText>{session.encounters.filter(encounter => !encounter.isJuv).length} adults [FIX ME!]</ListItemText></ListItem>
+		<ListItem><ListItemText>{session.encounters.filter(encounter => encounter.isJuv).length} juvs [FIX ME!]</ListItemText></ListItem>
+			</List>
+		<Table>
+			<TableHead>
+				<TableRow>
+						<TableCell>Species</TableCell>
+					<TableCell>New</TableCell>
+					<TableCell>Retraps</TableCell>
+					<TableCell>Adults</TableCell>
+					<TableCell>Juvs</TableCell>
+						</TableRow>
+			</TableHead>
+			<TableBody>
+				{speciesBreakdown.map(({ species, encounters }) => (
+					<TableRow key={species}>
+						<TableCell>{species}</TableCell>
+							<TableCell>{encounters.filter(encounter => encounter.recordType === 'N').length}</TableCell>
+						<TableCell>{encounters.filter(encounter => encounter.recordType === 'S').length}</TableCell>
+						<TableCell>{encounters.filter(encounter => !encounter.isJuv).length}</TableCell>
+						<TableCell>{encounters.filter(encounter => encounter.isJuv).length}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	</div>);
+}
 
 async function fetchSessionByDate(date: string) {
 	const { data }: GraphQLResponse<GetSessionByDateQuery> =
@@ -59,9 +117,7 @@ async function DisplayInitialData({
 		notFound();
 	}
 	return (
-		<div>
-			Session on {date} - {initialData?.encounters?.length} birds
-		</div>
+		<SessionSummary session={initialData} date={date} />
 	);
 }
 
