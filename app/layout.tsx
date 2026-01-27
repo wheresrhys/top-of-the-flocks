67d41d7efe4df0b508de6a441882ae2a30744af2
Rhys Evans <wheresrhys@gmail.com>
2026-01-27 16:33:05 +0000

    fix build error related to lazy loading flyonui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import GlobalNav from './components/GlobalNav';
 import LoadFlyonUI from './components/LoadFlyonUI';
+import { Suspense } from 'react';
 export const metadata: Metadata = {
 	title: 'Top of the Flocks',
 	description: 'Leaderboard for bird ringing data'
@@ -17,7 +18,9 @@ export default function RootLayout({
 			<body>
 				<GlobalNav />
 				{children}
-				<LoadFlyonUI />
+				<Suspense>
+					<LoadFlyonUI />
+				</Suspense>
 			</body>
 		</html>
 	);
