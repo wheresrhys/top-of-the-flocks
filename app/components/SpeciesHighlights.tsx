7a0db6a8eb825aa7449501ac2ec5a1917bbe0a8a
Rhys Evans <wheresrhys@gmail.com>
2026-02-02 11:30:20 +0000

    added oldest recent bird and styled species page highlight stats area

--- app/components/SpeciesHighlights.tsx
+++ app/components/SpeciesHighlights.tsx
@@ -1,11 +1,15 @@
 'use client';
-import { AccordionItem } from '@/app/components/Accordion';
 import { BoxyList } from '@/app/components/DesignSystem';
 import { useState } from 'react';
 import Link from 'next/link';
 import { format as formatDate } from 'date-fns';
 import type { PageData } from '@/app/(routes)/species/[speciesName]/page';
-import { type EnrichedBirdWithEncounters } from '@/app/(routes)/bird/[ring]/page';
+import {
+	orderBirdsByRecency,
+	type EnrichedBirdWithEncounters
+} from '@/app/lib/bird-data-helpers';
+import { StatOutput } from './StatOutput';
+
 function MostCaughtBird({
 	ring,
 	allBirds
@@ -42,79 +46,115 @@ function MostCaughtBird({
 	);
 }
 
+function getOldestBirds(
+	birds: EnrichedBirdWithEncounters[]
+): EnrichedBirdWithEncounters[] {
+	const oldestBirdAge = Math.max(...birds.map((bird) => bird.provenAge));
+	return oldestBirdAge > 1
+		? birds.filter((bird) => bird.provenAge === oldestBirdAge)
+		: [];
+}
+
 export function SpeciesHighlightStats({
 	topSessions,
 	birds,
 	mostCaughtBirds
 }: PageData) {
 	const [expandedId, setExpanded] = useState(false as string | false);
-	const oldestBirdAge = Math.max(...birds.map((bird) => bird.provenAge));
-	const oldestBirds =
-		oldestBirdAge > 1
-			? birds.filter((bird) => bird.provenAge === oldestBirdAge)
-			: [];
+	const oldestBirds = getOldestBirds(birds);
+	const oldestRecentBird = orderBirdsByRecency(
+		getOldestBirds(
+			birds.filter((bird) =>
+				bird.encounters.some(
+					(encounter) =>
+						new Date(encounter.session.visit_date) >
+						new Date(Date.now() - 1000 * 60 * 60 * 24 * 90)
+				)
+			)
+		),
+		'desc',
+		'last'
+	)[0];
 	return (
 		<BoxyList>
 			<li>{birds.length} individuals</li>
-			<li className="flex items-center gap-2 flex-wrap">
-				<span className="text-nowrap">
-					Oldest proven age: {oldestBirdAge} years old:
-				</span>
-				{oldestBirds.map((bird) => (
+			{oldestBirds.length ? (
+				<li className="flex items-center gap-2 flex-wrap">
+					<span className="text-nowrap">
+						Oldest birds: {oldestBirds[0].provenAge} years old:
+					</span>
+					{oldestBirds.map((bird) => (
+						<Link
+							key={bird.ring_no}
+							className="badge badge-outline link"
+							href={`/bird/${bird.ring_no}`}
+						>
+							{bird.ring_no}
+						</Link>
+					))}
+				</li>
+			) : (
+				<li>No notably old birds</li>
+			)}
+			{oldestRecentBird ? (
+				<li>
+					Oldest recent bird:{' '}
 					<Link
-						key={bird.ring_no}
 						className="badge badge-outline link"
-						href={`/bird/${bird.ring_no}`}
+						href={`/bird/${oldestRecentBird.ring_no}`}
 					>
-						{bird.ring_no}
+						{oldestRecentBird.ring_no}
+					</Link>{' '}
+					{oldestRecentBird.provenAge} years old, on{' '}
+					<Link
+						className="link"
+						href={`/session/${oldestRecentBird.encounters[oldestRecentBird.encounters.length - 1].session.visit_date}`}
+					>
+						{formatDate(
+							new Date(
+								oldestRecentBird.encounters[
+									oldestRecentBird.encounters.length - 1
+								].session.visit_date
+							),
+							'dd MMMM, yyyy'
+						)}
 					</Link>
+				</li>
+			) : null}
+			{/* todo: longest gap between first and last caught */}
+			{mostCaughtBirds?.length > 0 ? (
+				<li className="flex items-center gap-2 flex-wrap">
+					<span className="text-nowrap">
+						Most caught bird{mostCaughtBirds.length > 1 ? 's' : ''}:{' '}
+						{mostCaughtBirds[0].encounters} encounters
+					</span>
+					{mostCaughtBirds.map((bird) => (
+						<Link
+							key={bird.ring_no}
+							className="badge badge-outline link"
+							href={`/bird/${bird.ring_no}`}
+						>
+							{bird.ring_no}
+						</Link>
+					))}
+				</li>
+			) : (
+				<li>No birds retrapped</li>
+			)}
+			<li className="flex items-center gap-2 flex-wrap">
+				<span className="text-nowrap">Top sessions</span>
+				{topSessions.map((session) => (
+					<StatOutput
+						key={session.visit_date}
+						value={session.metric_value}
+						visitDate={session.visit_date}
+						temporalUnit="day"
+						unit="birds"
+						classes="badge badge-outline"
+						dateFormat="d MMM yyyy"
+					/>
 				))}
 			</li>
-
-			{mostCaughtBirds?.length > 0 ? (
-				<AccordionItem
-					key="most-caught-birds"
-					id="most-caught-birds"
-					HeadingComponent={() => <span>Most caught birds:</span>}
-					ContentComponent={() => (
-						<ol className="list-inside list-none">
-							{mostCaughtBirds.map((bird) => (
-								<MostCaughtBird
-									key={bird.ring_no}
-									ring={bird.ring_no}
-									allBirds={birds}
-								/>
-							))}
-						</ol>
-					)}
-					model={{}}
-					onToggle={setExpanded}
-					expandedId={expandedId}
-				/>
-			) : null}
-			<AccordionItem
-				key="top-sessions"
-				id="top-sessions"
-				HeadingComponent={() => <span>Top 5 sessions by encounters:</span>}
-				ContentComponent={() => (
-					<ol className="list-inside list-none">
-						{topSessions.map((session) => (
-							<li className="mb-2" key={session.visit_date}>
-								{session.metric_value} on{' '}
-								<Link className="link" href={`/session/${session.visit_date}`}>
-									{formatDate(
-										new Date(session.visit_date as string),
-										'dd MMMM, yyyy'
-									)}
-								</Link>
-							</li>
-						))}
-					</ol>
-				)}
-				model={{}}
-				onToggle={setExpanded}
-				expandedId={expandedId}
-			/>
 		</BoxyList>
 	);
 }
