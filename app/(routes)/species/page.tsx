8c79b07b636d7bf883c3eb5cdcc511e7cd198751
Rhys Evans <rhys.evans@ft.com>
2026-02-03 22:40:13 +0000

    use new SpeciesStats view on species page

--- app/(routes)/species/page.tsx
+++ app/(routes)/species/page.tsx
@@ -3,81 +3,78 @@ import type { Database } from '@/types/supabase.types';
 import Link from 'next/link';
 import { supabase, catchSupabaseErrors } from '@/lib/supabase';
 import { Table } from '@/app/components/DesignSystem';
-type SpeciesLeagueTableRow =
-	Database['public']['Views']['species_league_table']['Row'];
+type SpeciesStatsRow = Database['public']['Views']['SpeciesStats']['Row'];
 
-export async function fetchSpeciesData(): Promise<SpeciesLeagueTableRow[]> {
-	return supabase
-		.from('species_league_table')
-		.select(
-			`
-		species_name,
-		individuals,
-		encounters,
-		session_count,
-		longest_stay,
-		unluckiest,
-		longest_winged,
-		average_wing_length,
-		shortest_winged,
-		heaviest,
-		average_weight,
-		lightest,
-		total_weight
-	`
+type SpeciesStatsColumnConfig = {
+	label: string;
+	Component?: (value: string | number) => React.ReactNode;
+	accessor: keyof SpeciesStatsRow;
+};
+
+const speciesStatsColumns: SpeciesStatsColumnConfig[] = [
+	{
+		label: 'Species',
+		accessor: 'species_name',
+		// @ts-expect-error - TODO: fix this
+		Component: (speciesName: string) => (
+			<Link className="link text-wrap" href={`/species/${speciesName}`}>
+				{speciesName}
+			</Link>
 		)
-		.order('encounters', { ascending: false })
-		.then(catchSupabaseErrors) as Promise<SpeciesLeagueTableRow[]>;
+	},
+	{ label: 'Birds', accessor: 'bird_count' },
+	{ label: 'Encounters', accessor: 'encounter_count' },
+	{ label: 'Sessions', accessor: 'session_count' },
+	{ label: 'Max per session', accessor: 'max_per_session' },
+	{ label: '% Birds retrapped', accessor: 'pct_retrapped' },
+	{ label: 'Max time span', accessor: 'max_time_span' },
+	{ label: 'Max proven age', accessor: 'max_proven_age' },
+	{ label: 'Max encountered bird', accessor: 'max_encountered_bird' },
+	{ label: 'Max weight', accessor: 'max_weight' },
+	{ label: 'Avg weight', accessor: 'avg_weight' },
+	{ label: 'Min weight', accessor: 'min_weight' },
+	{ label: 'Median weight', accessor: 'median_weight' },
+	{ label: 'Max wing', accessor: 'max_wing' },
+	{ label: 'Avg wing', accessor: 'avg_wing' },
+	{ label: 'Min wing', accessor: 'min_wing' },
+	{ label: 'Median wing', accessor: 'median_wing' }
+];
+
+export async function fetchSpeciesData(): Promise<SpeciesStatsRow[]> {
+	return supabase
+		.from('SpeciesStats')
+		.select('*')
+		.order('encounter_count', { ascending: false })
+		.then(catchSupabaseErrors) as Promise<SpeciesStatsRow[]>;
 }
 
-function SpeciesLeagueTable({ data }: { data: SpeciesLeagueTableRow[] }) {
+function SpeciesStatsTable({ data }: { data: SpeciesStatsRow[] }) {
 	return (
 		<Table>
 			<thead>
 				<tr>
-					<th className="text-wrap">Species</th>
-					<th className="text-wrap">
-						<span className="hidden md:inline">Individuals</span>
-						<span className="inline md:hidden">Inds</span>
-					</th>
-					<th className="text-wrap">
-						<span className="hidden md:inline">Encounters</span>
-						<span className="inline md:hidden">Encs</span>
-					</th>
-					<th className="text-wrap">
-						<span className="hidden md:inline">Sessions</span>
-						<span className="inline md:hidden">Sess</span>
-					</th>
-					<th className="text-wrap">Max retraps</th>
-					<th className="text-wrap">Max Wing</th>
-					<th className="text-wrap">Avg Wing</th>
-					<th className="text-wrap">Min Wing</th>
-					<th className="text-wrap">Max Weight</th>
-					<th className="text-wrap">Avg Weight</th>
-					<th className="text-wrap">Min Weight</th>
+					{speciesStatsColumns.map((column) => (
+						<th className="text-wrap" key={column.accessor}>
+							{column.label}
+						</th>
+					))}
 				</tr>
 			</thead>
 			<tbody>
 				{data.map((species) => (
 					<tr key={species.species_name}>
-						<td>
-							<Link
-								className="link text-wrap"
-								href={`/species/${species.species_name}`}
-							>
-								{species.species_name}
-							</Link>
-						</td>
-						<td>{species.individuals}</td>
-						<td>{species.encounters}</td>
-						<td>{species.session_count}</td>
-						<td>{species.unluckiest}</td>
-						<td>{species.longest_winged}</td>
-						<td>{species.average_wing_length?.toFixed(1)}</td>
-						<td>{species.shortest_winged}</td>
-						<td>{species.heaviest}</td>
-						<td>{species.average_weight?.toFixed(1)}</td>
-						<td>{species.lightest}</td>
+						{speciesStatsColumns.map((column) => (
+							<td key={column.accessor}>
+								{column.Component
+									? column.Component(
+											/* @ts-expect-error - TODO: fix this */
+											species[column.accessor as keyof SpeciesStatsRow]
+										)
+									: (species[column.accessor as keyof SpeciesStatsRow] as
+											| string
+											| number)}
+							</td>
+						))}
 					</tr>
 				))}
 			</tbody>
@@ -87,10 +84,10 @@ function SpeciesLeagueTable({ data }: { data: SpeciesLeagueTableRow[] }) {
 
 export default async function AllSpeciesPage() {
 	return (
-		<BootstrapPageData<SpeciesLeagueTableRow[]>
+		<BootstrapPageData<SpeciesStatsRow[]>
 			getCacheKeys={() => ['species']}
 			dataFetcher={fetchSpeciesData}
-			PageComponent={SpeciesLeagueTable}
+			PageComponent={SpeciesStatsTable}
 		/>
 	);
 }
