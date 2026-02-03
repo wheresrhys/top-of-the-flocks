7684d158411648af21b64d6a8e54f43e9f49db77
Rhys Evans <wheresrhys@gmail.com>
2026-02-03 13:46:49 +0000

    --amend

--- app/(routes)/__tests__/home.test.jsx
+++ app/(routes)/__tests__/home.test.jsx
@@ -1,13 +1,20 @@
 import { expect, describe, it } from 'vitest';
-import { render, screen, getByRole, getAllByRole } from '@testing-library/react';
+import {
+	render,
+	screen,
+	getByRole,
+	getAllByRole
+} from '@testing-library/react';
 import Page from '../page';
 describe('home page', () => {
 	it('should render page content', async () => {
 		render(await Page());
-		const accordionGroups = await screen.findAllByTestId('stats-accordion-group');
+		const accordionGroups = await screen.findAllByTestId(
+			'stats-accordion-group'
+		);
 		accordionGroups.map((accordionGroup) => {
 			const heading = getByRole(accordionGroup, 'heading', {
-				level: 2,
+				level: 2
 			});
 			expect(heading.textContent).toMatch(/[a-z ]+:/i);
 			const buttons = getAllByRole(accordionGroup, 'button');
@@ -15,6 +22,5 @@ describe('home page', () => {
 				expect(button.textContent).toMatch(/[a-z ]+: [0-9]+ [a-z]+/i);
 			});
 		});
-
 	});
 });
