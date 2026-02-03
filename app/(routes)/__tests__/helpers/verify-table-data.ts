b3c3da49eeb5764ea49e4b6c357037446ffcad0f
Rhys Evans <wheresrhys@gmail.com>
2026-02-03 16:04:18 +0000

    --amend

--- app/(routes)/__tests__/helpers/verify-table-data.ts
+++ app/(routes)/__tests__/helpers/verify-table-data.ts
@@ -1,20 +1,20 @@
-import { getAllByRole } from "@testing-library/react";
-import { expect } from "vitest";
+import { getAllByRole } from '@testing-library/react';
+import { expect } from 'vitest';
 export function verifyTableData(table: HTMLElement, data: string[][]) {
-  const [thead, tbody] = getAllByRole(table, 'rowgroup');
-  const headers = getAllByRole(thead, 'columnheader');
-  expect(headers).toHaveLength(data[0].length);
-  data[0].map((header, index) => {
-    expect(headers[index].textContent.trim()).toBe(header);
-  });
-  const rowEls = getAllByRole(tbody, 'row');
-  expect(rowEls).toHaveLength(data.length - 1);
-  data.slice(1).map((row, index) => {
-    const rowEl = rowEls[index];
-    const cells = getAllByRole(rowEl, 'cell');
-    expect(cells).toHaveLength(row.length);
-    row.map((cell, index) => {
-      expect(cells[index].textContent.trim()).toBe(cell);
-    });
-  });
+	const [thead, tbody] = getAllByRole(table, 'rowgroup');
+	const headers = getAllByRole(thead, 'columnheader');
+	expect(headers).toHaveLength(data[0].length);
+	data[0].map((header, index) => {
+		expect(headers[index].textContent.trim()).toBe(header);
+	});
+	const rowEls = getAllByRole(tbody, 'row');
+	expect(rowEls).toHaveLength(data.length - 1);
+	data.slice(1).map((row, index) => {
+		const rowEl = rowEls[index];
+		const cells = getAllByRole(rowEl, 'cell');
+		expect(cells).toHaveLength(row.length);
+		row.map((cell, index) => {
+			expect(cells[index].textContent.trim()).toBe(cell);
+		});
+	});
 }
--- app/components/DesignSystem.tsx
+++ app/components/DesignSystem.tsx
@@ -7,24 +7,51 @@ export function PrimaryHeading({ children }: { children: React.ReactNode }) {
 export function SecondaryHeading({ children }: { children: React.ReactNode }) {
 	return <h2 className="text-base-content text-2xl mt-3 mb-3">{children}</h2>;
 }
-export function BoxyList({ children, testId }: { children: React.ReactNode, testId?: string }) {
+export function BoxyList({
+	children,
+	testId
+}: {
+	children: React.ReactNode;
+	testId?: string;
+}) {
 	return (
-		<ul data-testid={testId} className="border-base-content/25 divide-base-content/25 divide-y rounded-md border *:p-3 *:first:rounded-t-md *:last:rounded-b-md">
+		<ul
+			data-testid={testId}
+			className="border-base-content/25 divide-base-content/25 divide-y rounded-md border *:p-3 *:first:rounded-t-md *:last:rounded-b-md"
+		>
 			{children}
 		</ul>
 	);
 }
 
-export function Table({ children, testId }: { children: React.ReactNode, testId?: string }) {
+export function Table({
+	children,
+	testId
+}: {
+	children: React.ReactNode;
+	testId?: string;
+}) {
 	return (
 		<div className="w-full overflow-x-auto mt-4">
-			<table data-testid={testId} className="table">{children}</table>
+			<table data-testid={testId} className="table">
+				{children}
+			</table>
 		</div>
 	);
 }
 
-export function InlineTable({ children, testId }: { children: React.ReactNode, testId?: string }) {
-	return <table data-testid={testId} className="table table-xs">{children}</table>;
+export function InlineTable({
+	children,
+	testId
+}: {
+	children: React.ReactNode;
+	testId?: string;
+}) {
+	return (
+		<table data-testid={testId} className="table table-xs">
+			{children}
+		</table>
+	);
 }
 
 export function BadgeList({ items }: { items: string[] }) {
