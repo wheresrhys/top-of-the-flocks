8d96239763889f8597f15addc0cb28dd66820d07
Rhys Evans <wheresrhys@gmail.com>
2026-02-03 13:46:34 +0000

    completed home page test

--- app/(routes)/__tests__/home.test.jsx
+++ app/(routes)/__tests__/home.test.jsx
@@ -1,13 +1,20 @@
 import { expect, describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, getByRole, getAllByRole } from '@testing-library/react';
 import Page from '../page';
 describe('home page', () => {
-	it('should render the page', async () => {
+	it('should render page content', async () => {
 		render(await Page());
-		const heading = await screen.findByRole('heading', {
-			level: 2,
-			name: /Busiest sessions/
+		const accordionGroups = await screen.findAllByTestId('stats-accordion-group');
+		accordionGroups.map((accordionGroup) => {
+			const heading = getByRole(accordionGroup, 'heading', {
+				level: 2,
+			});
+			expect(heading.textContent).toMatch(/[a-z ]+:/i);
+			const buttons = getAllByRole(accordionGroup, 'button');
+			buttons.map((button) => {
+				expect(button.textContent).toMatch(/[a-z ]+: [0-9]+ [a-z]+/i);
+			});
 		});
-		expect(heading).toBeDefined();
+
 	});
 });
