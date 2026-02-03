781ad0a78e634ac2a0f03873093b3b3ac4913247
Rhys Evans <wheresrhys@gmail.com>
2026-02-03 16:43:20 +0000

    final few pages have basic tests

--- app/(routes)/__tests__/helpers/verify-table-data.ts
+++ app/(routes)/__tests__/helpers/verify-table-data.ts
@@ -1,6 +1,10 @@
 import { getAllByRole } from '@testing-library/react';
 import { expect } from 'vitest';
-export function verifyTableData(table: HTMLElement, data: string[][]) {
+export function verifyTableData(
+	table: HTMLElement,
+	data: string[][],
+	{ isPartial = true }: { isPartial?: boolean } = { isPartial: true }
+) {
 	const [thead, tbody] = getAllByRole(table, 'rowgroup');
 	const headers = getAllByRole(thead, 'columnheader');
 	expect(headers).toHaveLength(data[0].length);
@@ -8,7 +12,9 @@ export function verifyTableData(table: HTMLElement, data: string[][]) {
 		expect(headers[index].textContent.trim()).toBe(header);
 	});
 	const rowEls = getAllByRole(tbody, 'row');
-	expect(rowEls).toHaveLength(data.length - 1);
+	isPartial
+		? expect(rowEls.length).toBeGreaterThanOrEqual(data.length - 1)
+		: expect(rowEls).toHaveLength(data.length - 1);
 	data.slice(1).map((row, index) => {
 		const rowEl = rowEls[index];
 		const cells = getAllByRole(rowEl, 'cell');
--- app/(routes)/__tests__/sessions.test.jsx
+++ app/(routes)/__tests__/sessions.test.jsx
@@ -6,33 +6,54 @@ import {
 	getAllByRole,
 	fireEvent
 } from '@testing-library/react';
-import Page from '../session/page';
+import Page from '../sessions/page';
 
 describe('session list page', () => {
-	// it('should render session list', async () => {
-	//   render(await Page());
-	//   const accordionGroups = await screen.findAllByTestId(
-	//     'stats-accordion-group'
-	//   );
-	//   accordionGroups.map((accordionGroup) => {
-	//     const heading = getByRole(accordionGroup, 'heading', {
-	//       level: 2
-	//     });
-	//     expect(heading.textContent).toMatch(/[a-z ]+:/i);
-	//     const accordionWrapper = accordionGroup.querySelector(
-	//       ':scope > ul, :scope > ol'
-	//     );
-	//     getAllByRole(accordionGroup, 'button')
-	//       .filter((button) => {
-	//         const closestListItem = button.closest('li');
-	//         return (
-	//           closestListItem &&
-	//           closestListItem.parentElement === accordionWrapper
-	//         );
-	//       })
-	//       .forEach((button) => {
-	//         expect(button.textContent).toMatch(/^[a-z ]+: [0-9]+ [a-z]+$/i);
-	//       });
-	//   });
-	// });
+	it('should show correct heading and subheadings', async () => {
+		render(await Page());
+		const heading = await screen.findByRole('heading', {
+			level: 1
+		});
+		expect(heading.textContent).toBe('Session history');
+		const subheadings = await screen.findAllByRole('heading', {
+			level: 2
+		});
+		subheadings.map((heading, index) => {
+			expect(heading.textContent).toMatch(/\d{4}/);
+			if (index > 0) {
+				expect(heading.textContent).toBe(
+					String(Number(subheadings[index - 1].textContent) - 1)
+				);
+			}
+		});
+	});
+	it('should show session list accordions', async () => {
+		render(await Page());
+		const accordionGroups = await screen.findAllByTestId(
+			'history-accordion-group'
+		);
+		accordionGroups.map((accordionGroup) => {
+			const heading = getByRole(accordionGroup, 'heading', {
+				level: 2
+			});
+			const year = Number(heading.textContent);
+			const accordionWrapper = accordionGroup.querySelector(
+				':scope > ul, :scope > ol'
+			);
+			expect(accordionWrapper).toBeDefined();
+			getAllByRole(accordionGroup, 'button')
+				.filter((button) => {
+					const closestListItem = button.closest('li');
+					return (
+						closestListItem &&
+						closestListItem.parentElement === accordionWrapper
+					);
+				})
+				.forEach((button) => {
+					expect(button.textContent).toMatch(
+						/^[a-z]+: \d+ sessions, \d+ birds$/i
+					);
+				});
+		});
+	});
 });
