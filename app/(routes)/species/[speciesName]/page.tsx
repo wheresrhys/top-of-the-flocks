5e93bf9bec10704d08db1e6a2243a01677a90ce9
Rhys Evans <rhys.evans@ft.com>
2026-02-05 18:56:31 +0000

    moved bird list fetching into shared library

--- app/(routes)/species/[speciesName]/page.tsx
+++ app/(routes)/species/[speciesName]/page.tsx
@@ -5,18 +5,11 @@ import {
 	getTopPeriodsByMetric,
 	type TopPeriodsResult
 } from '@/app/isomorphic/stats-data-tables';
-import {
-	orderBirdsByRecency,
-	enrichBird,
-	type BirdWithEncounters,
-	type EnrichedBirdWithEncounters,
-	type Encounter
-} from '@/app/lib/bird-model';
+import { type EnrichedBirdWithEncounters } from '@/app/lib/bird-model';
 import { SpeciesPageWithFilters } from '@/app/components/SpeciesPageWithFilters';
-import { SPECIES_PAGE_BATCH_SIZE } from '@/app/isomorphic/constants';
+import { fetchPageOfBirds } from '@/app/isomorphic/single-species-data';
+
 type SpeciesStatsRow = Database['public']['Views']['SpeciesStats']['Row'];
-type PaginatedBirdsResult =
-	Database['public']['Functions']['paginated_birds_table']['Returns'][number];
 type PageParams = { speciesName: string };
 type PageProps = { params: Promise<PageParams> };
 
@@ -44,59 +37,10 @@ async function getSpeciesStats(species: string) {
 		.then(catchSupabaseErrors) as Promise<SpeciesStatsRow>;
 }
 
-function convertPaginatedBirdResultsToBirds(
-	paginatedBirdResults: PaginatedBirdsResult[]
-): BirdWithEncounters[] {
-	const birdsMap: Record<string, BirdWithEncounters> = {};
-	paginatedBirdResults.forEach((result) => {
-		if (!birdsMap[result.ring_no]) {
-			birdsMap[result.ring_no] = {
-				id: result.bird_id,
-				ring_no: result.ring_no,
-				encounters: [] as Encounter[]
-			} as BirdWithEncounters;
-		}
-		birdsMap[result.ring_no].encounters.push({
-			id: result.encounter_id,
-			capture_time: result.capture_time,
-			is_juv: result.is_juv,
-			minimum_years: result.minimum_years,
-			record_type: result.record_type,
-			sex: result.sex,
-			weight: result.weight,
-			wing_length: result.wing_length,
-			session: {
-				id: result.session_id,
-				visit_date: result.visit_date
-			}
-		} as Encounter);
-	});
-	return Object.values(birdsMap);
-}
-
-async function fetchAllBirds(species: string) {
-	const paginatedBirdResults = (await supabase
-		.rpc('paginated_birds_table', {
-			species_name_param: species,
-			result_limit: SPECIES_PAGE_BATCH_SIZE,
-			result_offset: 0
-		})
-		.then(catchSupabaseErrors)) as PaginatedBirdsResult[];
-
-	return orderBirdsByRecency<EnrichedBirdWithEncounters>(
-		convertPaginatedBirdResultsToBirds(paginatedBirdResults).map(enrichBird),
-		{
-			direction: 'desc',
-			type: 'last',
-			encountersAlreadySorted: true
-		}
-	);
-}
-
 async function fetchSpeciesData(params: PageParams): Promise<PageData | null> {
 	const [topSessions, birds, speciesStats] = await Promise.all([
 		getTopSessions(params.speciesName),
-		fetchAllBirds(params.speciesName),
+		fetchPageOfBirds(params.speciesName),
 		getSpeciesStats(params.speciesName)
 	]);
 	if (birds.length === 0) return null;
---
+++ app/isomorphic/single-species-data.ts
@@ -0,0 +1,62 @@
+import { SPECIES_PAGE_BATCH_SIZE } from '@/app/isomorphic/constants';
+import {
+	orderBirdsByRecency,
+	enrichBird,
+	type BirdWithEncounters,
+	type Encounter,
+	type EnrichedBirdWithEncounters
+} from '@/app/lib/bird-model';
+import { supabase, catchSupabaseErrors } from '@/lib/supabase';
+import type { Database } from '@/types/supabase.types';
+
+type PaginatedBirdsResult =
+	Database['public']['Functions']['paginated_birds_table']['Returns'][number];
+
+function convertPaginatedBirdResultsToBirds(
+	paginatedBirdResults: PaginatedBirdsResult[]
+): BirdWithEncounters[] {
+	const birdsMap: Record<string, BirdWithEncounters> = {};
+	paginatedBirdResults.forEach((result) => {
+		if (!birdsMap[result.ring_no]) {
+			birdsMap[result.ring_no] = {
+				id: result.bird_id,
+				ring_no: result.ring_no,
+				encounters: [] as Encounter[]
+			} as BirdWithEncounters;
+		}
+		birdsMap[result.ring_no].encounters.push({
+			id: result.encounter_id,
+			capture_time: result.capture_time,
+			is_juv: result.is_juv,
+			minimum_years: result.minimum_years,
+			record_type: result.record_type,
+			sex: result.sex,
+			weight: result.weight,
+			wing_length: result.wing_length,
+			session: {
+				id: result.session_id,
+				visit_date: result.visit_date
+			}
+		} as Encounter);
+	});
+	return Object.values(birdsMap);
+}
+
+export async function fetchPageOfBirds(species: string, page: number = 0) {
+	const paginatedBirdResults = (await supabase
+		.rpc('paginated_birds_table', {
+			species_name_param: species,
+			result_limit: SPECIES_PAGE_BATCH_SIZE,
+			result_offset: page * SPECIES_PAGE_BATCH_SIZE
+		})
+		.then(catchSupabaseErrors)) as PaginatedBirdsResult[];
+
+	return orderBirdsByRecency<EnrichedBirdWithEncounters>(
+		convertPaginatedBirdResultsToBirds(paginatedBirdResults).map(enrichBird),
+		{
+			direction: 'desc',
+			type: 'last',
+			encountersAlreadySorted: true
+		}
+	);
+}
